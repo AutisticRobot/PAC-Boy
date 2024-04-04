f47415e0a158576d423bdc954d5221e791c6654c
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-04-03 18:21:46 -0700

    added playSoundFile function to PAC-Boy

--- PACBoy.cpp
+++ PACBoy.cpp
@@ -3,29 +3,36 @@
 #include <chrono>
 #include <ctime>
 #include <map>
+#include <string>
 
-std::map<int, std::string> Alarms = 
-{
-    //{time, sound folder}
-};
+std::map<int, std::string> Alarms;//{time, sound folder}
 std::string getRandSoundFromFolder(std::string folder);
 void loadAlarmsFromFile(std::string File);
+void playSoundFile(std::string File);
 
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World! :]" << std::endl;
-    
+    std::cout << "Hello World! :]\n";
+    std::cout << Alarms[0][0] << std::endl;
 
     return 0;
 }
 
-std::string getRandSoundFromFolder(std::string folder)
+std::string getRandFileFromFolder(std::string folder)
 {
     return "";
 }
 
 void loadAlarmsFromFile(std::string File)
+{
+    Alarms = 
+    {
+        {300, "alarm"},
+    };
+}
+
+void playSoundFile(std::string File)
 {
 
 }
